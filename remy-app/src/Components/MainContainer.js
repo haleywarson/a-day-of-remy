20625d59ed8f2dcdc79a6abe7440d7797a4c6380
Haley Warson <haleywarson@gmail.com>
2021-04-20 09:29:54 -0600

    commit before merging

---
+++ remy-app/src/Components/MainContainer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import Kennel from './Kennel'
+import Kitchen from './Kitchen'
+import DogBed from './DogBed'
+
+export default function MainContainer(props) {
+
+    return (
+        <div className="main-container">
+            {/* if (props.kennel !== []) {
+                    return <Kennel kennel={props.kennel} remys={props.remys} kennelRemy={props.kennelRemy} />;
+                } else if (props.dogBed !== []) {
+                    return <DogBed dogBed={props.dogBed} remys={props.remys} napRemy={props.napRemy} />;
+                } else {
+                    return <Kitchen kitchen={props.kitchen} remys={props.remys} feedRemy={props.feedRemy} />;
+                } */}
+            <Kennel
+                kennel={props.kennel}
+                remys={props.remys}
+                kennelRemy={props.kennelRemy}
+            />
+            <DogBed
+                dogBed={props.dogBed}
+                remys={props.remys}
+                napRemy={props.napRemy}
+            />
+            <Kitchen
+                kitchen={props.kitchen}
+                remys={props.remys}
+                feedRemy={props.feedRemy}
+            />
+        </div>
+    );
+}
