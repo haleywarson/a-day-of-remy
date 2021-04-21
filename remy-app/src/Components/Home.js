1786146f9f1a18cca96c92e85d84bdc85e0b48e7
Haley Warson <haleywarson@gmail.com>
2021-04-21 13:09:47 -0600

    added dog nap timer functionality

--- remy-app/src/Components/Home.js
+++ remy-app/src/Components/Home.js
@@ -6,14 +6,15 @@ import "../index.css";
 import MainContainer from "./MainContainer";
 
 export default class App extends Component {
-
   state = {
     remys: [],
     kennel: [],
     dogBed: [],
     kitchen: [],
     dogPark: [],
-    treatCount: 0
+    treatCount: 0,
+    time: 0,
+    clockId: null,
   };
 
   componentDidMount() {
@@ -25,6 +26,10 @@ export default class App extends Component {
           kennel: remys[0],
         })
       );
+  }
+
+  componentWillUnmount() {
+    this.stopClock();
   }
 
   feedRemy = (remy) => {
@@ -66,57 +71,83 @@ export default class App extends Component {
 
   giveTreats = () => {
     this.setState({
-      treatCount: this.state.treatCount + 1
+      treatCount: this.state.treatCount + 1,
     });
-  }
+  };
 
-  // imFullAlert = () => {
-  //   if ({this.state.treatCount} >== 5) {
-  //     alert("I'm full!")
-  //   }
-  // }
+  imFullAlert = () => {
+    if (this.state.treatCount === 5) {
+      alert("I'm full!");
+    }
+  };
+
+  startClock = () => {
+    const clockId = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+    this.setState({
+      clockId,
+    });
+  };
+
+  resetTime = () => {
+    this.setState({
+      time: 0,
+    });
+  };
+  
+  stopClock = () => {
+    if (this.state.clockId) {
+      clearInterval(this.state.clockId);
+      this.resetTime();
+    }
+  };
 
   render() {
-        return (
-          <div className="App">
-            <header></header>
-            <main>
-              <MainContainer
-                kennel={this.state.kennel}
-                dogBed={this.state.dogBed}
-                kitchen={this.state.kitchen}
-                dogPark={this.state.dogPark}
-                remys={this.state.remys}
-                feedRemy={this.feedRemy}
-                napRemy={this.napRemy}
-                kennelRemy={this.kennelRemy}
-                parkRemy={this.parkRemy}
-                giveTreats={this.giveTreats}
-                treatCount={this.state.treatCount}
-              />
-            </main>
-            <footer>
-              <p>
-                Created By:{" "}
-                <a
-                  href="https://github.com/haleywarson"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Haley
-                </a>{" "}
-                and{" "}
-                <a
-                  href="https://github.com/Kwayzaar"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Eric
-                </a>
-              </p>
-            </footer>
-          </div>
-        );
-    }
+    return (
+      <div className="App">
+        <header></header>
+        <main>
+          <MainContainer
+            kennel={this.state.kennel}
+            dogBed={this.state.dogBed}
+            kitchen={this.state.kitchen}
+            dogPark={this.state.dogPark}
+            remys={this.state.remys}
+            feedRemy={this.feedRemy}
+            napRemy={this.napRemy}
+            kennelRemy={this.kennelRemy}
+            parkRemy={this.parkRemy}
+            giveTreats={this.giveTreats}
+            treatCount={this.state.treatCount}
+            imFullAlert={this.imFullAlert}
+            startClock={this.startClock}
+            stopClock={this.stopClock}
+            time={this.state.time}
+          />
+        </main>
+        <footer>
+          <p>
+            Created By:{" "}
+            <a
+              href="https://github.com/haleywarson"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Haley
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://github.com/Kwayzaar"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Eric
+            </a>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
--- remy-app/src/Components/Kitchen.js
+++ remy-app/src/Components/Kitchen.js
@@ -11,19 +11,20 @@ export default function Kitchen(props) {
         let remyInKitchen = props.kitchen.length !== 0;
         if(remyInKitchen) {
                 return (
-                <>
-                    <RemyCard
-                        kitchen={props.kitchen}
-                        remys={props.remys}
-                        feedRemy={props.feedRemy}
-                        napRemy={props.napRemy}
-                        parkRemy={props.parkRemy}
-                        kennelRemy={props.kennelRemy}
-                    />
-                    <button onClick={props.giveTreats}>Feed me</button>
-                    <p>{props.treatCount} treats</p>
-                </>
-                );
+                    <>
+                        <RemyCard
+                            kitchen={props.kitchen}
+                            remys={props.remys}
+                            feedRemy={props.feedRemy}
+                            napRemy={props.napRemy}
+                            parkRemy={props.parkRemy}
+                            kennelRemy={props.kennelRemy}
+                        />
+                        <button onClick={props.giveTreats}>Good boy!</button>
+                        <p>{props.treatCount} treats</p>
+                        {props.imFullAlert()}
+                    </>
+                    );
         }
     }
 
