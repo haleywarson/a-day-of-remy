ae51fd0651c7614fd8c43476c6c73193115d18e1
Haley Warson <haleywarson@gmail.com>
2021-04-22 07:12:15 -0600

    friend cards displaying properly. still need styling.

--- remy-app/src/Components/Friends.js
+++ remy-app/src/Components/Friends.js
@@ -5,15 +5,29 @@ import "../index.css";
 
 export default function Friends() {
     return (
-        <div className="friends-container">
-                <ul className="friend-list">
-                        <li className="friend-card">Friend 1</li>
-                        <img alt="Friend 1" src="" />
-                        <li className="friend-card">Friend 2</li>
-                        <img alt="Friend 2" src="" />
-                        <li className="friend-card">Friend 3</li>
-                        <img alt="Friend 3" src="" />
-                </ul>
-        </div>
+      <div className="friends-container">
+        <ul className="friends-list">
+          <div className="friend-card-container">
+            <li className="friend-card">
+              Friend 1
+              <img alt="Friend 1" src="" />
+            </li>
+          </div>
+
+          <div className="friend-card-container">
+            <li className="friend-card">
+              Friend 2
+              <img alt="Friend 2" src="" />
+            </li>
+          </div>
+
+          <div className="friend-card-container">
+            <li className="friend-card">
+              Friend 3
+              <img alt="Friend 3" src="" />
+            </li>
+          </div>
+        </ul>
+      </div>
     );
 }
