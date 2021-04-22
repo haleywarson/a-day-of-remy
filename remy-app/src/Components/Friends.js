a9e5e47b1107f8f54d966219b250c53fc9db7bc0
Haley Warson <haleywarson@gmail.com>
2021-04-22 10:31:28 -0600

    commit before merge

--- remy-app/src/Components/Friends.js
+++ remy-app/src/Components/Friends.js
@@ -5,29 +5,29 @@ import "../index.css";
 
 export default function Friends() {
     return (
-      <div className="friends-container">
-        <ul className="friends-list">
-          <div className="friend-card-container">
-            <li className="friend-card">
-              Friend 1
-              <img alt="Friend 1" src="" />
-            </li>
-          </div>
+        <div className="friends-container">
+            <ul className="friends-list">
+            <div className="friend-card-container">
+                <li className="friend-card">
+                Friend 1
+                <img alt="Friend 1" src="" />
+                </li>
+            </div>
 
-          <div className="friend-card-container">
-            <li className="friend-card">
-              Friend 2
-              <img alt="Friend 2" src="" />
-            </li>
-          </div>
+            <div className="friend-card-container">
+                <li className="friend-card">
+                Friend 2
+                <img alt="Friend 2" src="" />
+                </li>
+            </div>
 
-          <div className="friend-card-container">
-            <li className="friend-card">
-              Friend 3
-              <img alt="Friend 3" src="" />
-            </li>
-          </div>
-        </ul>
-      </div>
+            <div className="friend-card-container">
+                <li className="friend-card">
+                Friend 3
+                <img alt="Friend 3" src="" />
+                </li>
+            </div>
+            </ul>
+        </div>
     );
 }
